1778924afa9a07896b6b116a8f6cbdcc519ebb26
avi-jit <avijit.thawani@gmail.com>
2024-09-21 22:13:08 -0700

    favicon

---
+++ src/app/v0/Favicon.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Book } from 'lucide-react';
+
+const Favicon: React.FC = () => (
+  <link
+    rel="icon"
+    href="/static/book-a.svg" // Update this path to the correct location of your static file
+  />
+);
+
+export default Favicon;
